7a15264b36b201abceed169374dc51bb36373da7
fardeen15 <fardeenmemon17@gmail.com>
2019-07-20 00:51:22 -0700

    Updates

---
+++ precache-manifest.1503bbc260222aaf6b6a3c204cda75a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b170ab5a902de38948ad593e54b32a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5b9807e9e5ad99e9c1a",
+    "url": "/static/css/main.b98838d1.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "c5b9807e9e5ad99e9c1a",
+    "url": "/static/js/main.0543a60c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
